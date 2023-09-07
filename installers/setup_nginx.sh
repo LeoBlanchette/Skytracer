d740ce5cb0b5a802413c990af6f671485ad26876
Leo <theswitchboard@protonmail.com>
2023-09-07 18:27:09 -0500

    Removed unecessary file.

--- installers/setup_nginx.sh
+++ installers/setup_nginx.sh
@@ -7,9 +7,6 @@ REPLACEONIONSITE=$($HOME/Skytracer/Bin/./gethostname )
 
 echo $REPLACEONIONSITE
 
-sudo cp -f $HOME/Skytracer/installers/configfiles/django.conf /etc/nginx/conf.d/
-
-sudo sed -i 's|{REPLACEONIONSITE}|'"${REPLACEONIONSITE}"'|g' /etc/nginx/conf.d/django.conf
 sudo sed -i 's|# server_names_hash_bucket_size 64|'"server_names_hash_bucket_size 512"'|g' /etc/nginx/nginx.conf
 
 sudo cp -f $HOME/Skytracer/installers/configfiles/nginx-default /etc/nginx/sites-available/onionsite
