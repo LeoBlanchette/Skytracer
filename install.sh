7fdf2c4407068f2e2658e366fdc5fd3dec36648a
Leo <theswitchboard@protonmail.com>
2023-09-07 16:41:36 -0500

    Installation corrections

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 echo Installing Skytracer...
+#Updates and upgrades...
 sudo apt update
 sudo apt upgrade
 
+#Main installations...
 installers/./install_main_dependencies.sh
 installers/./install_tor.sh
 installers/./install_python_modules.sh
 
-#general setup
+#General setup...
 installers/./setup_gunicorn.sh
 installers/./setup_nginx.sh
 installers/./setup_services.sh
+
+#Post notes...
+installers/./postnotes.sh
+
---
+++ installers/postnotes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+HOSTNAME=$($HOME/Skytracer/Bin/./gethostname)
+echo ""; echo ""; 
+
+echo "Remember, The Tor browser is required to view Onion sites:"
+echo "https://www.torproject.org/download/"
+
+echo ""
+
+echo "Your skytracer web address is:"
+echo $HOSTNAME
+
+echo ""
